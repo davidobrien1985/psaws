3880446505f773856bcdfe9c787c9b98f7777906
David OBrien <me@david-obrien.net>
2017-01-25 13:25:31 +1100

    Add initial version of module with only connect function

---
+++ psaws.psm1
@@ -0,0 +1,29 @@
+﻿Function Connect-AwsMfa {
+  param (
+    [string]$region = 'ap-southeast-2',
+    [string]$awsProfile = 'default',
+    [Parameter(mandatory=$true,HelpMessage='Specify the user name associated with this profile and MFA token')]
+    [string]$awsUserName,
+    [Parameter(mandatory=$true,HelpMessage='Specify the MFA Token that is currently valid')]
+    [string]$mfaToken
+  )
+  
+  $parameters = @{
+    'Region' = $region
+    'ProfileName' = $awsProfile
+    'UserName' = $awsUserName
+  }
+
+  $mfaDeviceArn = (Get-IAMMFADevice @parameters).SerialNumber
+  
+  $parameters = @{
+    DurationInSeconds = 900
+    TokenCode = $mfaToken
+    SerialNumber = $mfaDeviceArn
+    ProfileName = $awsProfile
+    Region = $region
+  }
+  
+  $sts = Get-STSSessionToken -DurationInSeconds 900 -SerialNumber $mfaDeviceArn -TokenCode $mfaToken -Region $region -ProfileName $awsProfile -Verbose
+  $sts
+}
