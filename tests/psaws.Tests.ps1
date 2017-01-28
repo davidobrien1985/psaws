627db3ec57267351b59d8ddb1ff8197c3a5d2233
David OBrien <me@david-obrien.net>
2017-01-29 09:03:35 +1100

    Fix some formatting stuff

--- tests/psaws.Tests.ps1
+++ tests/psaws.Tests.ps1
@@ -1,4 +1,4 @@
-﻿$ModuleName = "psaws"
+﻿$ModuleName = 'psaws'
 
 # http://www.lazywinadmin.com/2016/05/using-pester-to-test-your-manifest-file.html
 # Make sure one or multiple versions of the module are not loaded
@@ -12,36 +12,36 @@ $ModuleInformation = Import-Module -Name $ManifestFile -PassThru
 
 
 Describe "$ModuleName Module - Testing Manifest File (.psd1)"{
-    Context "Manifest"{
-        It "Should contain RootModule"{
+    Context 'Manifest'{
+        It 'Should contain RootModule'{
             $ModuleInformation.RootModule | Should not BeNullOrEmpty
         }
-        It "Should contain Author"{
+        It 'Should contain Author'{
             $ModuleInformation.Author | Should not BeNullOrEmpty
         }
-        It "Should contain Company Name"{
+        It 'Should contain Company Name'{
             $ModuleInformation.CompanyName | Should not BeNullOrEmpty
         }
-        It "Should contain Description"{
+        It 'Should contain Description'{
             $ModuleInformation.Description | Should not BeNullOrEmpty
         }
-        It "Should contain Copyright"{
+        It 'Should contain Copyright'{
             $ModuleInformation.Copyright | Should not BeNullOrEmpty
         }
-        It "Should contain License"{
+        It 'Should contain License'{
             $ModuleInformation.LicenseURI | Should not BeNullOrEmpty
         }
-        It "Should contain a Project Link"{
+        It 'Should contain a Project Link'{
             $ModuleInformation.ProjectURI | Should not BeNullOrEmpty
         }
-        It "Should contain Tags (For the PSGallery)"{
+        It 'Should contain Tags (For the PSGallery)'{
             $ModuleInformation.Tags.count | Should not BeNullOrEmpty
         }
     }
 }
 
 InModuleScope 'psaws' {
-  Describe "psaws" {
+  Describe 'psaws' {
     Context 'Testing Connect-AwsMfa' {
       Mock -CommandName Get-IAMMFADevice -MockWith {
         return @{
@@ -57,15 +57,15 @@ InModuleScope 'psaws' {
         }
       }
   
-      It "should not accept MFA Tokens shorter than 6 digits" {
+      It 'should not accept MFA Tokens shorter than 6 digits' {
         {Connect-AwsMfa -region ap-southeast-2 -awsUserName me@david-obrien.net -awsProfile default -mfaToken 12345} | Should throw
       }
   
-      It "should not accept MFA Tokens longer than 6 digits" {
+      It 'should not accept MFA Tokens longer than 6 digits' {
         {Connect-AwsMfa -region ap-southeast-2 -awsUserName me@david-obrien.net -awsProfile default -mfaToken 1234567} | Should throw
       }
     
-      It "should accept known AWS regions (Jan 2017)" {
+      It 'should accept known AWS regions (Jan 2017)' {
         $regions = Get-AWSRegion
         foreach ($region in $regions) {
           $region
@@ -73,11 +73,11 @@ InModuleScope 'psaws' {
         }
       }
     
-      It "should throw with unknown AWS region" {
+      It 'should throw with unknown AWS region' {
         {Connect-AwsMfa -region 'ant-verycold-1' -awsUserName me@david-obrien.net -awsProfile default -mfaToken 123456} | Should throw
       }
   
-      It "checks the output of Connect-AwsMfa" {
+      It 'checks the output of Connect-AwsMfa' {
         Connect-AwsMfa -region ap-southeast-2 -awsUserName me@david-obrien.net -awsProfile default -mfaToken 123456 | Should BeOfType Hashtable
         (Connect-AwsMfa -region ap-southeast-2 -awsUserName me@david-obrien.net -awsProfile default -mfaToken 123456).AccessKey | Should BeExactly 'ASIAIRAOHDFIYJBKF4GA'
         (Connect-AwsMfa -region ap-southeast-2 -awsUserName me@david-obrien.net -awsProfile default -mfaToken 123456).SecretKey | Should BeExactly 'LC8/e/IfDbLHBhnVkIEfhFgaA4/FQel8FLRuUrXeS'
